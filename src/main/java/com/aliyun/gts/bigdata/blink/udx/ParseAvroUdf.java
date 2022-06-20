63c8c3f6168f8c7ba470ab12bbd910a0094b693e
高矩 <gaoju.zxh@alibaba-inc.com>
2022-06-21 00:09:21 +0800

    提交解析avro数据函数

--- src/main/java/com/aliyun/gts/bigdata/blink/udx/ParseAvroUdf.java
+++ src/main/java/com/aliyun/gts/bigdata/blink/udx/ParseAvroUdf.java
@@ -1,14 +1,11 @@
 package com.aliyun.gts.bigdata.blink.udx;
 
 import org.apache.avro.Schema;
-import org.apache.avro.file.DataFileReader;
-import org.apache.avro.file.SeekableByteArrayInput;
 import org.apache.avro.generic.GenericDatumReader;
 import org.apache.avro.generic.GenericRecord;
 import org.apache.avro.io.BinaryDecoder;
 import org.apache.avro.io.DatumReader;
 import org.apache.avro.io.DecoderFactory;
-import org.apache.avro.io.JsonDecoder;
 import org.apache.flink.table.functions.FunctionContext;
 import org.apache.flink.table.functions.ScalarFunction;
 
